b16a3382bd0dd7db062e6960acef1d2650c3e5f8
Rodrigo Siqueira Silva <rodrigosiqueirasilva@hotmail.com>
2026-03-27 07:22:32 -0300

    Criando domain

---
+++ FIAPCloudGames/FIAP.PosTech.ArqSistemas.CloudGames.Domain/PessoaFisica.cs
@@ -0,0 +1,11 @@
+﻿namespace FIAP.PosTech.ArqSistemas.CloudGames.Domain
+{
+    public class PessoaFisica
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public required string Email { get; set; }
+        public required string Senha { get; set; }
+        public byte Administrador { get; set; }
+    }
+}
---
+++ FIAPCloudGames/FIAP.PosTech.ArqSistemas.CloudGames.Domain/Promocao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP.PosTech.ArqSistemas.CloudGames.Domain
+{
+    public class Promocao
+    {
+        public int Id { get; set; }
+        public required string Descricao { get; set; }
+        public byte Ativa { get; set; }
+    }
+}
