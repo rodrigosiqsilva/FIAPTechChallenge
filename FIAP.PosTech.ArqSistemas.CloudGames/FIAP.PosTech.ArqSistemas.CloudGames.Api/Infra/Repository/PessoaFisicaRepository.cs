e61b33083a0e35b1492c21d9e1d128f572a63079
Rodrigo Siqueira Silva <rodrigosiqueirasilva@hotmail.com>
2026-04-07 14:35:17 -0300

    Evolução Entity

---
+++ FIAP.PosTech.ArqSistemas.CloudGames/FIAP.PosTech.ArqSistemas.CloudGames.Api/Infra/Repository/PessoaFisicaRepository.cs
@@ -0,0 +1,12 @@
+﻿using FIAP.PosTech.ArqSistemas.CloudGames.Api.Interfaces;
+using FIAP.PosTech.ArqSistemas.CloudGames.Domain.Model;
+
+namespace FIAP.PosTech.ArqSistemas.CloudGames.Api.Infra.Repository
+{
+    public class PessoaFisicaRepository : CrudServiceRepository<PessoaFisica>, IPessoaFisicaRepository
+    {
+        public PessoaFisicaRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
